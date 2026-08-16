e2393a0931a4c8fbbd1f9d7ae1b6d95b053f54d5
alek-core <alekcore35@gmail.com>
2026-08-16 18:25:46 -0500

    🔧 :wrench: configurar eslint para análisis estático del código

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        console: 'readonly'
+      }
+    }
+  },
+  {
+    ignores: ['node_modules/', 'coverage/']
+  }
+]
